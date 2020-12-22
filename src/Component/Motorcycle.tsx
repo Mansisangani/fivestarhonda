014cf8cee180ccd4d59d339281ceaf95c34716ce
kunj-sangani <Kunj@1212>
2020-12-22 23:52:17 +0530

    Back Link Update

--- src/Component/Motorcycle.tsx
+++ src/Component/Motorcycle.tsx
@@ -16,12 +16,12 @@ export const Motorcycle: React.FunctionComponent<IMotorcycleProps> = (props: Rea
       {
         motorId ?
           <div style={{ marginTop: 15 }}>
-            <span
+            <Link to={`/motorcycle`}><span
               style={{ fontWeight: 'bold', cursor: 'pointer', fontSize: 18 }}
               onClick={() => { window.location.href = "/motorcycle"; }}>
               <img style={{ width: 25 }} src="data:image/svg+xml;utf8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCI+PGcgZGF0YS1uYW1lPSJMYXllciAyIj48ZyBkYXRhLW5hbWU9ImFycm93LWJhY2siPjxyZWN0IHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgb3BhY2l0eT0iMCIgdHJhbnNmb3JtPSJyb3RhdGUoOTAgMTIgMTIpIi8+PHBhdGggZD0iTTE5IDExSDcuMTRsMy42My00LjM2YTEgMSAwIDEgMC0xLjU0LTEuMjhsLTUgNmExLjE5IDEuMTkgMCAwIDAtLjA5LjE1YzAgLjA1IDAgLjA4LS4wNy4xM0ExIDEgMCAwIDAgNCAxMmExIDEgMCAwIDAgLjA3LjM2YzAgLjA1IDAgLjA4LjA3LjEzYTEuMTkgMS4xOSAwIDAgMCAuMDkuMTVsNSA2QTEgMSAwIDAgMCAxMCAxOWExIDEgMCAwIDAgLjY0LS4yMyAxIDEgMCAwIDAgLjEzLTEuNDFMNy4xNCAxM0gxOWExIDEgMCAwIDAgMC0yeiIvPjwvZz48L2c+PC9zdmc+"></img>
               {`Go Back`}
-            </span>
+            </span></Link>
             <ProdcutDetail ProductNumber={moterNumber} ProductType={"Motorcycle"}></ProdcutDetail>
           </div>
           : Data.motorCycle.map((val) => {
